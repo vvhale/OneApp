b4e5ed53cd3f9294db9b5ccc524033bf5eb92860
kazantsevib <kazantsevib@asu3.lok.ru>
2014-02-07 11:01:41 +0400

    третий ком

--- OneApp/src/ru/veloanapa/oneapp/OneApp.java
+++ OneApp/src/ru/veloanapa/oneapp/OneApp.java
@@ -2,21 +2,35 @@ package ru.veloanapa.oneapp;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.Menu;
+import android.view.View;
 
 public class OneApp extends Activity {
 // первый комит
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_one_app);
+		setContentView(new DrawView(this));
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+class DrawView extends View{
+	public DrawView(Context context){
+		super(context);
+	}
+protected void onDraw(Canvas canvas){
+	canvas.drawColor(Color.BLUE);
+}
+	
+}
+	
+	
+	//@Override
+	//public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.one_app, menu);
-		return true;
-	}
+	//	getMenuInflater().inflate(R.menu.one_app, menu);
+	//	return true;
+	//}
 
 }
